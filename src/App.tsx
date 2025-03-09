e56dba233e18141f28ca6f26e487430cc7d9b842
clinton reynolds <eastwood188209@gmail.com>
2025-03-09 13:09:44 -0400

    updeted file structure and implemented dependencies

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer.tsx";
+import Header from "./components/Header/Header.tsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>Hello World</p>
+      <Footer />
+    </>
+  );
+}
+
+export default App;

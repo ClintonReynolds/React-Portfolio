c8102d9b6d264ce1b317d8dace860e62236bf818
clinton reynolds <eastwood188209@gmail.com>
2025-03-24 19:54:26 -0400

    added link 2

--- src/componets/Work/WorkItem.jsx
+++ src/componets/Work/WorkItem.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const WorkItem = ({screenshot, description, link}) => {
+const WorkItem = ({screenshot, description, link, link2}) => {
   return (
     <div className="grid-item">
     <img src={screenshot} alt={description} className="screenshot" />
     <h3>{description}</h3>
     <a href={link} target="_blank" rel="noopener noreferrer">
+      View Project
+    </a>
+    <a href={link2} target="_blank" rel="noopener noreferrer">
       View Project
     </a>
     </div>
--- src/componets/Work/index.jsx
+++ src/componets/Work/index.jsx
@@ -13,36 +13,42 @@ const previousWorks = [
       id: 1,
       description: 'Prework Study Guide',
       link: 'https://github.com/ClintonReynolds/prework-study-guide.git',
+      link2: 'https://weather-dashboard-1k00.onrender.com/',
       screenshot: pic01,
     },
     {
       id: 2,
       description: 'GIFT-EXCHANGE',
       link: 'https://github.com/JayC-06/GIFT-EXCHANGE-.git',
+      link2: 'https://weather-dashboard-1k00.onrender.com/',
       screenshot: pic02,
     },
     {
       id: 3,
       description: 'Weather Dashboard',
       link: 'https://github.com/ClintonReynolds/weather-dashboard.git',
+      link2: 'https://weather-dashboard-1k00.onrender.com/',
       screenshot: pic03,
     },
     {
       id: 4,
       description: 'README Generator',
       link: 'https://github.com/ClintonReynolds/readme-generator.git',
+      link2: 'https://weather-dashboard-1k00.onrender.com/',
       screenshot: pic04,
     },
     {
       id: 5,
       description: 'Vehicle Builder',
       link: 'https://github.com/ClintonReynolds/Vehicle-Builder.git',
+      link2: 'https://weather-dashboard-1k00.onrender.com/',
       screenshot: pic05,
     },
     {
       id: 6,
       description: 'Employee Tracker',
       link: 'https://github.com/ClintonReynolds/Employee-Tracker.git',
+      link2: 'https://weather-dashboard-1k00.onrender.com/',
       screenshot: pic06,
     },
     
@@ -56,6 +62,7 @@ const Work = () => {
           key={work.id}
           description={work.description}
           link={work.link}
+          link2={work.link2}
           screenshot={work.screenshot}
         />
       ))}
